83be83f7ea037cc0628e4bfed025ccd315ca63f8
shecho <shechovala@gmail.com>
2024-12-08 12:58:17 -0500

    add links

--- resources/views/home.php
+++ resources/views/home.php
@@ -16,8 +16,8 @@
     </div>
       <h5 class="card-title">Bienvenido al Aplicativo Web</h5>
       <p class="card-text">Analisis Metrologico Biomedico</p>
-      <a href="/Login" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Iniciar Sesión</a>
-      <a href="/Login" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Crear Cuenta</a>
+      <a href="/login" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Iniciar Sesión</a>
+      <a href="/register" class="btn btn-primary active" data-bs-toggle="button" aria-pressed="true">Crear Cuenta</a>
       <br><br>
       <a href="/products" class="btn btn-primary">Ir a Menú</a>
     </div>

68a66426bf2b04f01be0922af814ece123a121a2
shecho <shechovala@gmail.com>
2024-12-10 08:10:39 -0500

    fix import

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\registerController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\MovementController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -11,7 +11,5 @@ Route::get("/", HomeController::class)->name("home");
 Route::resource("/products",ProductController::class);
 Route::get("/poducts/pdf/{id}",[ProductController::class, "pdf"])->name("pdf");
 //Route::resource("/movements", MovementController::class);
-
-
-Route::resource("/login",LoginController::class);
-Route::resource("/register",LoginController::class);
+Route::resource("/login",LoginController::class)->name("login");
+Route::resource("/register",RegisterController::class)->name("register");
